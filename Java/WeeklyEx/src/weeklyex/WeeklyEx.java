7de744992d1c76626726010fbdf2e0d31430b901
Verna Koskinen <darth.kipsu@gmail.com>
2014-06-27 22:26:49 +0300

    add exercise 26 part 2

--- Java/WeeklyEx/src/weeklyex/WeeklyEx.java
+++ Java/WeeklyEx/src/weeklyex/WeeklyEx.java
@@ -11,24 +11,40 @@ import java.util.ArrayList;
 /**
  * This is week 27 exercise. The task is to take a string as input
  * and transform it to a list with each word added in it's own
- * index. Don't use string methods to archive this.
+ * index. Don't use string manipulation methods.
+ * 
+ * This is called finite state machine.
  */
 public class WeeklyEx {
     
     public static void createList(String input) {
         ArrayList<String> list = new ArrayList();
+        boolean attached = false;
         String word = "";
         for (int i=0; i<input.length(); i++) {
             switch(input.charAt(i)) {
                 case ' ': 
-                    if (!"".equals(word)) list.add(word);
-                    word = "";
+                    if (!attached) {
+                        if (word.length()>0) list.add(word);
+                        word = "";
+                    } else {
+                        word += input.charAt(i);
+                    }
+                    break;
+                case '"':
+                    if (!attached) {
+                        attached = true;
+                    } else {
+                        attached = false;
+                        if (word.length()>0) list.add(word);
+                        word = "";
+                    }
                     break;
                 default:
                     word += input.charAt(i);
             }            
         }
-        if (!"".equals(word)) list.add(word);
+        if (word.length()>0) list.add(word);
         System.out.println(list.toString());
     }
 
@@ -37,8 +53,10 @@ public class WeeklyEx {
      */
     public static void main(String[] args) {
         String string1 = "Viikko harjoitus viikolle numero 27";
+        String string2 = "Tämä string sisältää \"kaksi sanaa\" samassa";
         
         createList(string1);
+        createList(string2);
     }
     
 }
